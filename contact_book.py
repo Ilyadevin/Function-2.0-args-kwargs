450ee15690e39cbb283dc233ac3a4e28552cb1c1
Ilya Erlingas <Aligruit@gmail.com>
2019-12-28 19:00:19 +0300

    First corrections

--- contact_book.py
+++ contact_book.py
@@ -1,33 +1,67 @@
 class Contact:
-    def __init__(self, first_name, surname, phone_number, favorite="нет", **kwargs):
-        self.first_name = first_name
-        self.surname = surname
-        self.phone_number = phone_number
-        self.favorite = favorite
-        self.keys_list = []
-        self.values_list = []
-        for key, value in kwargs.items():
-            self.key = key
-            self.keys_list.append(self.key)
-            self.value = value
-            self.values_list.append(self.value)
+    contacts = {}
 
-    def str(self):
-        print("Имя:", self.first_name)
-        print("Фамилия:", self.surname)
-        print("Номер телефона:", self.phone_number)
-        print("В избранных:", self.favorite)
+    def __init__(self, *args, favorite="Нет", **kwargs):
+        name = args[0]
+        surname = args[1]
+        phone_number = args[2]
+        favorite_1 = favorite
+        kwargs_dict = {}
+        print("Имя:", args[0])
+        print("Фамилия:", args[1])
+        print("Номер телефона:", args[2])
+        print("В избранных:", favorite)
         print("Дополнительная информация:")
-        print("                     ", self.keys_list[0], ":", self.values_list[0])
-        print("                     ", self.keys_list[1], ":", self.values_list[1])
+        for key, value in kwargs.items():
+            print("         ", key, ":", value)
+        self.contacts[name] = [name, surname, phone_number, favorite_1, kwargs_dict]
 
 
 jhon = Contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
-print(jhon.str())
+print(jhon)
 
 
 class PhoneBook(Contact):
-    def __init__(self, first_name, surname, phone_number, **kwargs):
-        super().__init__(first_name, surname, phone_number, **kwargs)
-        self.phone_book_name = self.phone_book_name
-        
+    PhoneBook = "Contacts and Data"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def adding(self):
+        while True:
+            first_name_new = input("Введите имя ")
+            surname_new = input("Введите фамилию ")
+            phone_number_new = input("Введите номер телефона ")
+            favorites = input("Внести в избранные?(Y/N) ")
+            if favorites == "Y":
+                favorites = "Да"
+            else:
+                favorites = "Нет"
+            telegram_new = input("Имя пользователя Telegram начиная с @ ")
+            email_new = input("Введите адрес электронной почты ")
+            other = Contact(first_name_new, surname_new, phone_number_new, favorites, telegram=telegram_new,
+                            email=email_new)
+            print(other)
+
+    @classmethod
+    def __delitem__(cls, phone_number):
+        phone_number = input("Введите номер телефона ")
+        if phone_number == Contact(phone_number):
+            print("1")
+
+    @classmethod
+    def find(cls, favorite, contacts):
+        if favorite == "Да":
+            print(contacts)
+    @classmethod
+    def find_by_name(cls, name, surname,  contacts):
+        name_1 = input("Введите имя")
+        surname_1 = input("Введите фамилию")
+        if name_1 and surname_1 == name and surname:
+            print(contacts)
+
+
+
+q = PhoneBook("q", "q", "798", "1", "q")
+del q["798"]
+print("DONE!")
